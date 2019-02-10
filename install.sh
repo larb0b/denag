a20c7a308e2ab2cbbd56003720b50af6e64e9b06
Larkin Nickle <me@larbob.org>
2019-02-10 02:10:28 -0500

    Add scripts

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+if [ `id -u` -ne 0 ]; then
+   echo "Must be run as root." 
+   exit 1
+fi
+echo "Copying denag to /usr/local/bin"
+cp denag /usr/local/bin &&
+echo "Creating /usr/denag"
+mkdir -p /usr/denag &&
+echo "Populating /usr/denag with symlinks to driverwrap"
+ln -sf /usr/lib/driverwrap /usr/denag/CC &&
+ln -sf /usr/lib/driverwrap /usr/denag/c89 &&
+ln -sf /usr/lib/driverwrap /usr/denag/cc &&
+echo "Replacing symlinks in /usr/bin"
+ln -sf /usr/local/bin/denag /usr/bin/CC &&
+ln -sf /usr/local/bin/denag /usr/bin/c89 &&
+ln -sf /usr/local/bin/denag /usr/bin/cc &&
+echo "Done!"
---
+++ remove.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if [ `id -u` -ne 0 ]; then
+   echo "Must be run as root."
+   exit 1
+fi
+echo "Removing /usr/denag"
+rm -rf /usr/denag &&
+echo "Deleting /usr/local/bin/denag"
+rm /usr/local/bin/denag &&
+echo "Restoring original /usr/bin symlinks"
+ln -sf /usr/lib/driverwrap /usr/bin/CC &&
+ln -sf /usr/lib/driverwrap /usr/bin/c89 &&
+ln -sf /usr/lib/driverwrap /usr/bin/cc &&
+echo "Done!"
