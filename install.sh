3bf1b81ec977b52b8cb1fb44fa142573d34cb8b7
Larkin Nickle <me@larbob.org>
2019-02-10 15:54:38 -0500

    Improve scripts

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
+set -e
 if [ `id -u` -ne 0 ]; then
    echo "Must be run as root." 
    exit 1
 fi
-echo "Creating /usr/local/bin"
-mkdir -p /usr/local/bin &&
+if [ ! -d "/usr/local/bin" ]; then
+	echo "Creating /usr/local/bin"
+	mkdir -p /usr/local/bin 
+fi
 echo "Copying denag to /usr/local/bin"
-cp denag /usr/local/bin &&
+cp denag /usr/local/bin 
 echo "Creating /usr/denag"
-mkdir -p /usr/denag &&
+mkdir -p /usr/denag 
 echo "Populating /usr/denag with symlinks to driverwrap"
-ln -sf /usr/lib/driverwrap /usr/denag/CC &&
-ln -sf /usr/lib/driverwrap /usr/denag/c89 &&
-ln -sf /usr/lib/driverwrap /usr/denag/cc &&
+ln -sf /usr/lib/driverwrap /usr/denag/CC 
+ln -sf /usr/lib/driverwrap /usr/denag/c89 
+ln -sf /usr/lib/driverwrap /usr/denag/cc 
 echo "Replacing symlinks in /usr/bin"
-ln -sf /usr/local/bin/denag /usr/bin/CC &&
-ln -sf /usr/local/bin/denag /usr/bin/c89 &&
-ln -sf /usr/local/bin/denag /usr/bin/cc &&
+ln -sf /usr/local/bin/denag /usr/bin/CC 
+ln -sf /usr/local/bin/denag /usr/bin/c89 
+ln -sf /usr/local/bin/denag /usr/bin/cc 
 echo "Done!"
--- remove.sh
+++ remove.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+set -e
 if [ `id -u` -ne 0 ]; then
    echo "Must be run as root."
    exit 1
 fi
 echo "Removing /usr/denag"
-rm -rf /usr/denag &&
+rm -rf /usr/denag 
 echo "Deleting /usr/local/bin/denag"
-rm /usr/local/bin/denag &&
+rm /usr/local/bin/denag 
 echo "Restoring original /usr/bin symlinks"
-ln -sf /usr/lib/driverwrap /usr/bin/CC &&
-ln -sf /usr/lib/driverwrap /usr/bin/c89 &&
-ln -sf /usr/lib/driverwrap /usr/bin/cc &&
+ln -sf /usr/lib/driverwrap /usr/bin/CC 
+ln -sf /usr/lib/driverwrap /usr/bin/c89 
+ln -sf /usr/lib/driverwrap /usr/bin/cc 
 echo "Done!"
